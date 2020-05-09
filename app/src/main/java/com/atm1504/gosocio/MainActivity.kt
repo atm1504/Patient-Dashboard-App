0fef1f49d7b26653a683c9aab164de4f677b2600
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-05-09 21:12:54 +0530

    minor fix

--- app/src/main/java/com/atm1504/gosocio/MainActivity.kt
+++ app/src/main/java/com/atm1504/gosocio/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_signup,
+                R.id.nav_home, R.id.nav_login, R.id.nav_signup,
                 R.id.nav_patient_profile, R.id.nav_share, R.id.nav_reports_submitted
             ), drawerLayout
         )
